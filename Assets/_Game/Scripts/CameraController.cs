0780423c4f63b6a57e097360630e07a7d6fa63d0
James Faloon <faloonj@sheridancollege.ca>
2026-09-16 23:32:18 -0400

    final product

--- Assets/_Game/Scripts/CameraController.cs
+++ Assets/_Game/Scripts/CameraController.cs
@@ -1,25 +1,62 @@
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Cameras;
+using Unity.Cinemachine;
+using UnityEngine.InputSystem;
 
+ 
 public class CameraController : MonoBehaviour
 {
 
-    public List<Camera> cameras;
 
+
+    // Each Cinemachine camera 
+    public List<CinemachineCamera> cameras;
+    private int currentCameraIndex = 0; 
+    
+    // the camera input actions used 
+    private InputActionMap cameraActionMap;
+    private InputAction switchAction;
+
+
+    // on awake set find the switchAction from the Camera Action Map and enable it 
     void Awake()
     {
-        cameras = new List<Camera>(GetComponentsInChildren<Camera>());
+        cameraActionMap = InputSystem.actions.FindActionMap("Camera");
+        switchAction = cameraActionMap.FindAction("Switch");
+        switchAction?.Enable();
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // adjust Piroirty of cameras so the follow camera is first 
     void Start()
     {
+        foreach (var camera in cameras) 
+        {
+            camera.Priority = 0;
+        }
+        if(cameras.Count > 0)
+        {
+            cameras[currentCameraIndex % cameras.Count].Priority = 10;
+        }
 
     }
 
-    // Update is called once per frame
+    // if the switch action button was pressed that frame switch cameras 
     void Update()
     {
+
+        if (cameras.Count == 0 || switchAction == null) return;
+
+        // Only on the frame the button goes down. Reading the value instead
+        // would cycle the cameras every frame the button is held.
+        bool switched = switchAction.WasPressedThisFrame();
+
+        if (switched)
+        {
+            cameras[currentCameraIndex].Priority = 0;
+            currentCameraIndex = (currentCameraIndex + 1) % cameras.Count;
+            cameras[currentCameraIndex].Priority = 10;
+        }
+
 
     }
 }
--- Assets/_Game/Scripts/MainPlayerController.cs
+++ Assets/_Game/Scripts/MainPlayerController.cs
@@ -6,37 +6,72 @@ using UnityEngine.InputSystem;
 public class MainPlayerController : MonoBehaviour
 {
 
-
-
+    // all fields related to the Input Actions
     private InputAction moveAction;
     private CharacterController characterController;
     private Vector2 move;
-
     [SerializeField]
     private float speed = 5f;
 
+    // all fields related to animation 
+    private Animator animator;
 
+
+    // a method design to track each time the camera does a transformation within the game i.e when the app goes behind the Boss to the Side 
+    [SerializeField]
+    private Transform cameraTransform;
+
+
+    // on awake find the move input action 
     void Awake()
     {
-        // This is a very important line of code. It allows us to access the Input System actions that we created in the Input Actions asset.
         moveAction = InputSystem.actions.FindAction("Move");
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+
+
+        //if the postion of the camera is not set and there is a main camera set it 
+        if (cameraTransform == null && Camera.main != null)
+        {
+            cameraTransform = Camera.main.transform;
+            Debug.Log(cameraTransform);
+        }
 
         moveAction.Enable();
     }
 
-    // Update is called once per frame
+    // check if there is a move action and move based on the postion of the camera 
     void Update()
     {
         move = moveAction.ReadValue<Vector2>();
 
-        Vector3 deltaMove = new Vector3(move.x, 0f, move.y) * speed * Time.deltaTime;
+        Vector3 deltaMove = CameraRelative(move) * speed * Time.deltaTime;
 
         characterController.Move(deltaMove);
+    }
+
+    // Used to move character based on Camera rather then world 
+    private Vector3 CameraRelative(Vector2 input)
+    {
+
+        // if no camera is assigned for whatever reason just moved based on the direction of the world 
+        if (cameraTransform == null)
+        {
+            return new Vector3(input.x, 0f, input.y);
+        }
+
+        // how far forward should we move 
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+
+
+        // return how the character should move based on the camera and the input values 
+        return right * input.x + forward * input.y;
     }
 }
